8c2137de06ad0e20b6dca48c8e959fde013f98c5
almakamilia <almakamilia27@gmail.com>
2023-04-26 20:31:57 +0700

    inisial deployment

---
+++ kadarair.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+import numpy as np
+
+st.write("Perhitungan Kadar Air Sampel") 
+
+
+Bobot_awal_sampel = st.number_input("Masukkan bobot awal sampel (g) - ") 
+Bobot_konstan_sampel = st.number_input("Masukkan bobot konstan (g)")
+
+
+tombol=st.button("Hitung Kadar air sampel")
+if tombol:
+    hitung_kadar_air_sampel = ((Bobot_awal_sampel - Bobot_konstan_sampel/Bobot_awal_sampel)*100)
+    st.success(f"persentase kadar air sampel (%) sebesar... {round (hitung_kadar_air_sampel)}")
+
+    st.balloons()
